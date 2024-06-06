42470a16152eb23a561936427a41c9b267a58aac
erykksc <48735008+erykksc@users.noreply.github.com>
2024-06-07 00:24:59 +0200

    Add debugging and lualine config

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,23 +9,23 @@ return {
 			vim.g.copilot_no_tab_map = true
 		end,
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "github/copilot.lua" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		opts = {
-			debug = true, -- Enable debugging
-			-- See Configuration section for rest
-		},
-		-- Quick chat with Copilot
-		vim.keymap.set("n", "<leader>ccq", function()
-			local input = vim.fn.input("Quick Chat: ")
-			if input ~= "" then
-				require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-			end
-		end, { desc = "CopilotChat - Quick chat" }),
-	},
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	branch = "canary",
+	-- 	dependencies = {
+	-- 		{ "github/copilot.lua" }, -- or github/copilot.vim
+	-- 		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	-- 	},
+	-- 	opts = {
+	-- 		debug = false, -- Enable debugging
+	-- 		-- See Configuration section for rest
+	-- 	},
+	-- 	-- Quick chat with Copilot
+	-- 	vim.keymap.set("n", "<leader>ccq", function()
+	-- 		local input = vim.fn.input("Quick Chat: ")
+	-- 		if input ~= "" then
+	-- 			require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+	-- 		end
+	-- 	end, { desc = "CopilotChat - Quick chat" }),
+	-- },
 }
