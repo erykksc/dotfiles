3d89a0849d90f1b51e82f3a95a6c653db51d8be3
erykksc <48735008+erykksc@users.noreply.github.com>
2024-06-22 00:18:08 +0200

    Configure nvim copilot chat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,23 +9,33 @@ return {
 			vim.g.copilot_no_tab_map = true
 		end,
 	},
-	-- {
-	-- 	"CopilotC-Nvim/CopilotChat.nvim",
-	-- 	branch = "canary",
-	-- 	dependencies = {
-	-- 		{ "github/copilot.lua" }, -- or github/copilot.vim
-	-- 		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-	-- 	},
-	-- 	opts = {
-	-- 		debug = false, -- Enable debugging
-	-- 		-- See Configuration section for rest
-	-- 	},
-	-- 	-- Quick chat with Copilot
-	-- 	vim.keymap.set("n", "<leader>ccq", function()
-	-- 		local input = vim.fn.input("Quick Chat: ")
-	-- 		if input ~= "" then
-	-- 			require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-	-- 		end
-	-- 	end, { desc = "CopilotChat - Quick chat" }),
-	-- },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = {
+			debug = false,
+			auto_insert_mode = true,
+		},
+		-- Quick chat with Copilot
+		vim.keymap.set({ "n", "x" }, "<leader>aq", function()
+			local input = vim.fn.input("Quick Chat: ")
+			if input ~= "" then
+				require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+			end
+		end, { desc = "[A]sk quick chat" }),
+
+		vim.keymap.set({ "n", "x" }, "<leader>at", "<cmd>CopilotChatToggle<CR>", { desc = "[A]sk Copilot [T]oggle" }),
+		vim.keymap.set({ "n", "x" }, "<leader>ar", "<cmd>CopilotChatReview<CR>", { desc = "[A]sk for [R]eview" }),
+		vim.keymap.set({ "n", "x" }, "<leader>ae", "<cmd>CopilotChatExplain<CR>", { desc = "[A]sk to [E]xplain" }),
+		vim.keymap.set(
+			{ "n", "x" },
+			"<leader>ac",
+			"<cmd>CopilotChatCommitStaged<CR>",
+			{ desc = "[A]sk for [C]ommit Message of Staged" }
+		),
+	},
 }
