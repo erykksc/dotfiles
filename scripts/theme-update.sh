7c9ffba5422a989beead8f8a55ea77ab69424352
erykksc <48735008+erykksc@users.noreply.github.com>
2024-07-12 19:30:35 +0200

    Remove: kitty from theme-update.sh

--- scripts/theme-update.sh
+++ scripts/theme-update.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Source .zprofile if it exists
-if [ -f ~/.zprofile ]; then
-  source ~/.zprofile
-fi
+# if [ -f ~/.zprofile ]; then
+#   source ~/.zprofile
+# fi
 
-# ALACRITTY_CONFIG_FILE="$XDG_CONFIG_HOME/alacritty/alacritty.toml"
-# LIGHT_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/themes/catppuccin_latte.toml"
-# DARK_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/themes/catppuccin.toml"
-LIGHT_KITTY_THEME="Catppuccin-Latte"
-DARK_KITTY_THEME="Catppuccin-Mocha"
+ALACRITTY_CONFIG_FILE="$XDG_CONFIG_HOME/alacritty/alacritty.toml"
+LIGHT_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/themes/catppuccin_latte.toml"
+DARK_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/themes/catppuccin.toml"
 
+# LIGHT_KITTY_THEME="Catppuccin-Latte"
+# DARK_KITTY_THEME="Catppuccin-Mocha"
+#
 YAZI_THEME_FILE="$XDG_CONFIG_HOME/yazi/theme.toml"
 YAZI_LIGHT_FLAVOR="catppuccin-latte"
 YAZI_DARK_FLAVOR="catppuccin-mocha"
-
-echo $YAZI_THEME_FILE
 
 # Check if there is user override
 if [ "$1" == "dark" ]; then
@@ -36,18 +35,18 @@ fi
 
 if [ "$APPLY_THEME" == "dark" ]; then
     # Switch to dark theme
-#     sed -i '' -e "/import = \[/,/\]/c\\
-# import = [\\
-#     \"$DARK_ALACRITTY_THEME\",\\
-# ]" "$ALACRITTY_CONFIG_FILE"
-    kitty +kitten themes --cache-age 1 --reload-in=all $DARK_KITTY_THEME
+    sed -i '' -e "/import = \[/,/\]/c\\
+import = [\\
+    \"$DARK_ALACRITTY_THEME\",\\
+]" $ALACRITTY_CONFIG_FILE
     sed -i '' -e "s/^use=.*/use=\"$YAZI_DARK_FLAVOR\"/g" $YAZI_THEME_FILE
+    # kitty +kitten themes --cache-age 1 --reload-in=all $DARK_KITTY_THEME
 else
     # Switch to light theme
-#     sed -i '' -e "/import = \[/,/\]/c\\
-# import = [\\
-#     \"$LIGHT_ALACRITTY_THEME\",\\
-# ]" "$ALACRITTY_CONFIG_FILE"
-    kitty +kitten themes --cache-age 1 --reload-in=all $LIGHT_KITTY_THEME
+    sed -i '' -e "/import = \[/,/\]/c\\
+import = [\\
+    \"$LIGHT_ALACRITTY_THEME\",\\
+]" $ALACRITTY_CONFIG_FILE
     sed -i '' -e "s/^use=.*/use=\"$YAZI_LIGHT_FLAVOR\"/g" $YAZI_THEME_FILE
+    # kitty +kitten themes --cache-age 1 --reload-in=all $LIGHT_KITTY_THEME
 fi
