21c6c0a64cd528700c3e96af7f5c4786a9cb5e83
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-08-12 17:47:17 +0200

    Change: from catppuccin theme to github and alacritty to kitty

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,17 @@
 return {
 	{
-		"catppuccin/nvim",
+		"projekt0n/github-nvim-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					harpoon = true,
-					mason = true,
-				},
-			})
+			vim.cmd("colorscheme github_dark")
 		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"navarasu/onedark.nvim",
 	},
 	{
 		"f-person/auto-dark-mode.nvim",
@@ -17,12 +20,15 @@ return {
 		opts = {
 			update_interval = 1000,
 			set_dark_mode = function()
-				vim.opt.background = "dark"
-				vim.cmd("colorscheme catppuccin-mocha")
+				-- vim.opt.background = "dark"
+				-- vim.cmd("colorscheme catppuccin-mocha")
+				-- vim.cmd("colorscheme kanagawa")
+				vim.cmd("colorscheme github_dark")
 			end,
 			set_light_mode = function()
-				vim.opt.background = "light"
-				vim.cmd("colorscheme catppuccin-latte")
+				-- vim.opt.background = "light"
+				-- vim.cmd("colorscheme catppuccin-latte")
+				vim.cmd("colorscheme github_light")
 			end,
 		},
 	},
--- scripts/theme-update.sh
+++ scripts/theme-update.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Source .zprofile if it exists
-# if [ -f ~/.zprofile ]; then
-#   source ~/.zprofile
-# fi
-
 ALACRITTY_CONFIG_FILE="$XDG_CONFIG_HOME/alacritty/alacritty.toml"
-LIGHT_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/catppuccin_latte.toml"
-DARK_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/catppuccin.toml"
+LIGHT_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/github_light.toml"
+DARK_ALACRITTY_THEME="$XDG_CONFIG_HOME/alacritty/themes/github_dark.toml"
 
-# LIGHT_KITTY_THEME="Catppuccin-Latte"
-# DARK_KITTY_THEME="Catppuccin-Mocha"
+LIGHT_KITTY_THEME="GitHub Light"
+DARK_KITTY_THEME="GitHub Dark Dimmed"
 #
 YAZI_THEME_FILE="$XDG_CONFIG_HOME/yazi/theme.toml"
 YAZI_LIGHT_FLAVOR="catppuccin-latte"
@@ -32,23 +27,22 @@ fi
 
 
 # Apply the theme
-
 if [ "$APPLY_THEME" == "dark" ]; then
     # Switch to dark theme
-    sed -i '' -e "/import = \[/,/\]/c\\
-import = [\\
-    \"$DARK_ALACRITTY_THEME\",\\
-]" $ALACRITTY_CONFIG_FILE
-    sed -i '' -e "s/^use=.*/use=\"$YAZI_DARK_FLAVOR\"/g" $YAZI_THEME_FILE
-    # kitty +kitten themes --cache-age 1 --reload-in=all $DARK_KITTY_THEME
-    echo '--theme="Catppuccin Mocha"' > $XDG_CONFIG_HOME/bat/config
+#     sed -i '' -e "/import = \[/,/\]/c\\
+# import = [\\
+#     \"$DARK_ALACRITTY_THEME\",\\
+# ]" $ALACRITTY_CONFIG_FILE
+    # sed -i '' -e "s/^use=.*/use=\"$YAZI_DARK_FLAVOR\"/g" $YAZI_THEME_FILE
+    # echo '--theme="Catppuccin Mocha"' > $XDG_CONFIG_HOME/bat/config
+    kitty +kitten themes --cache-age 1 --reload-in=all $DARK_KITTY_THEME
 else
     # Switch to light theme
-    sed -i '' -e "/import = \[/,/\]/c\\
-import = [\\
-    \"$LIGHT_ALACRITTY_THEME\",\\
-]" $ALACRITTY_CONFIG_FILE
-    sed -i '' -e "s/^use=.*/use=\"$YAZI_LIGHT_FLAVOR\"/g" $YAZI_THEME_FILE
-    # kitty +kitten themes --cache-age 1 --reload-in=all $LIGHT_KITTY_THEME
-    echo '--theme="Catppuccin Latte"' > $XDG_CONFIG_HOME/bat/config
+#     sed -i '' -e "/import = \[/,/\]/c\\
+# import = [\\
+#     \"$LIGHT_ALACRITTY_THEME\",\\
+# ]" $ALACRITTY_CONFIG_FILE
+    # sed -i '' -e "s/^use=.*/use=\"$YAZI_LIGHT_FLAVOR\"/g" $YAZI_THEME_FILE
+    # echo '--theme="Catppuccin Latte"' > $XDG_CONFIG_HOME/bat/config
+    kitty +kitten themes --cache-age 1 --reload-in=all $LIGHT_KITTY_THEME
 fi
