a15f8b5fe085a9054119d0c6f9bedd41381201cc
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-08-12 17:42:10 +0200

    Add: config for treesitter textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
+		"nvim-treesitter/nvim-treesitter", -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+		-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
@@ -26,11 +27,53 @@ return {
 						["<leader>pc"] = "@class.outer",
 					},
 				},
+				select = {
+					enable = true,
+					keymaps = {
+						["if"] = "@function.inner",
+						["af"] = "@function.outer",
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+					},
+					selection_modes = {
+						-- ["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						-- ["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = true,
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]a"] = "@parameter.inner",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+					},
+				},
 			},
 		},
 		config = function(_, opts)
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup(opts)
+
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 		end,
 	},
 	{
