fc44657e601c71d0fa9c1ea7c4f0941dd4fbfdd5
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-08-12 18:13:13 +0200

    Remove: nvim keymaps that became defaults in v0.10

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,19 +82,11 @@ vim.o.softtabstop = 4
 -- Number of spaces that a <Tab> counts for while editing
 vim.o.shiftwidth = 4
 
--- Enable True Color
-if vim.fn.has("termguicolors") == 1 then
-	vim.opt.termguicolors = true
-end
-
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
