a67c1c850c8bbf40290ce542575afa4df2549478
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-09-11 16:51:34 +0200

    Update: netrw config in nvim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,10 +3,9 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
--- disable netrw at the very start of your init.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
 vim.g.netrw_banner = 0
+vim.g.netrw_preview = 1
+vim.g.netrw_liststyle = 3
 
 -- Make line numbers default
 vim.opt.number = true
