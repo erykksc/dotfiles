6a3cb57a045608e1c8ae765711515676e442b726
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-09-28 17:08:44 +0200

    Add: argument for specifying session to tmux-client.sh
    Additionally, now one can use tmux-client.sh inside
    tmux sessions.

--- scripts/tmux-client.sh
+++ scripts/tmux-client.sh
@@ -1,16 +1,30 @@
 #!/bin/zsh
 
-# Get the current directory name
-dir_name=$(basename "$PWD")
+# Get the directory path
+if [ -z "$1" ]; then
+	dir_path=$PWD
+else
+	dir_path="$1"
+fi
 
-# Start home directory tmux session if it doesn't exist
-output=$(tmux new-session -A -s $dir_name -c $PWD)
+dir_name=$(basename "$dir_path")
+dir_name="${dir_name//./_}"
+
+tmux new-session -d -s $dir_name -c $dir_path
+
+
+if [ -n "$TMUX" ]; then
+	#Already inside tmux session
+	output=$(tmux switch-client -t $dir_name)
+else
+	output=$(tmux a -t $dir_name)
+fi
 
 # If tmux session is exited, attach to the next one
 # If none exist, quit
 while true; do
-    if [[ "$output" != "[exited]" ]]; then
-        exit 0
-    fi
-    output=$(tmux a)
+	if [[ "$output" != "[exited]" ]]; then
+		exit 0
+	fi
+	output=$(tmux a)
 done
