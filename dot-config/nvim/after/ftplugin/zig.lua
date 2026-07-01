450d06b2361c2bbe592782eb3f3465d855d2c1b7
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2026-07-01 11:38:11 +0200

    feat(nvim): add config for zig and disable lint plugin

---
+++ dot-config/nvim/after/ftplugin/zig.lua
@@ -0,0 +1,3 @@
+-- Zig format guide recommends to aim for 100 characters line width
+-- and use common sense
+vim.bo.textwidth = 100
--- dot-config/nvim/lua/erykksc/lint.lua
+++ dot-config/nvim/lua/erykksc/lint.lua
@@ -1,14 +1,14 @@
 -- plugin: nvim-lint
 vim.pack.add({ "https://github.com/mfussenegger/nvim-lint" })
 
-require("lint").linters_by_ft = {
-	yaml = { "redocly" },
-}
-
-local try_lint = function()
-	require("lint").try_lint({}, {
-		ignore_errors = true,
-	})
-end
-vim.api.nvim_create_autocmd({ "BufWritePost" }, { callback = try_lint })
-vim.api.nvim_create_autocmd({ "BufReadPost" }, { callback = try_lint })
+-- require("lint").linters_by_ft = {
+-- 	yaml = { "redocly" },
+-- }
+--
+-- local try_lint = function()
+-- 	require("lint").try_lint({}, {
+-- 		ignore_errors = true,
+-- 	})
+-- end
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, { callback = try_lint })
+-- vim.api.nvim_create_autocmd({ "BufReadPost" }, { callback = try_lint })
